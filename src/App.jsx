9fd13fe1e8e58b2e22ecc132d6541d3c3d199053
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2025-12-20 11:48:12 +0600

    Implement Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import About from "./components/About";
 
 const App = () => {
   return (
-    <div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
-    </div>
+      <About />
+    </main>
   );
 };
 
