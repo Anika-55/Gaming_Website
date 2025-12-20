a7e230c365f4962d34f2d3f0ad1d85a8206c33b5
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2025-12-20 23:24:10 +0600

    Implement contact and story section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 import About from "./components/About";
 import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Contact />
     </main>
   );
 };
